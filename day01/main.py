b11a9092528ddc49c1df1b752defd88ba9fcfc8b
Aditya Diwakar <aditya@diwakar.io>
2022-12-01 00:27:38 -0500

    add comment for day 1

--- day01/main.py
+++ day01/main.py
@@ -1,6 +1,8 @@
 # open file, parse lines as integers
 f = open("input.txt", "r").read().strip().split("\n\n")
+# compute total number of calories for each elf
 elf_sums = map(lambda chunk: sum(map(int, chunk)), [c.split("\n") for c in f])
+# sort calories descending
 elf_sums_sorted = sorted(elf_sums, reverse=True)
 # part 1
 print(elf_sums_sorted[0])
